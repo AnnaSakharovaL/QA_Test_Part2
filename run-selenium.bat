47f75e1ffd5987c3ba812afed44de252978985fc
AnnaVikhristiuk <AnnaSakharova89@gmail.com>
2019-11-23 19:35:39 +0300

    Delete run-selenium.bat

--- run-selenium.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-IF [%1] == [] (
-    ECHO Specify path to chrome drive in 1st parameter of script
-    EXIT /B 1
-)
-
-java -Dwebdriver.chrome.driver="%1" -jar vendor\se\selenium-server-standalone\bin\selenium-server-standalone.jar
